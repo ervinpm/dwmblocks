ea4017faec8e5497612072e4d5eee0f4e98a9dde
Ervin Marasigan <ervinpm@gmail.com>
2022-02-20 07:21:37 -0600

    feat: added volume, memory and battery level

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{" ", "cat /sys/class/power_supply/BAT1/capacity",					30,		0},
+	{"墳 ", "amixer get Master | tail -n1 | awk -F '[][]' '{print $2}'",					30,		0},
+
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
