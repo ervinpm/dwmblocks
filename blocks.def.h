64eebaeff0b3ffd653b15c6ba5be339e8b4ae0a1
Ervin Marasigan <ervinpm@gmail.com>
2022-02-26 15:57:08 -0600

    feat: added colors to status bar

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "echo $(cat /sys/class/power_supply/BAT1/capacity)%",					30,		0},
-	{"墳 ", "amixer get Master | tail -n1 | awk -F '[][]' '{print $2}'",	30,		30},
-	{"ﯧ ", "xbacklight | awk -F '.' '{print $1 \"%\"}'",	0,		11},
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	    30,		0},
+	{"^c#98C379^  ", "echo $(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -c 5-)^d^",					5,		0},
+	{"^c#E06C75^ ", "echo $(cat /sys/class/power_supply/BAT1/capacity)%^d^",					30,		0},
+	{"^c#E5C07B^墳 ", "echo $(amixer get Master | tail -n1 | awk -F '[][]' '{print $2}')^d^",	30,		30},
+	{"^c#C678DD^ﯧ ", "echo $(xbacklight | awk -F '.' '{print $1}')%^d^",	0,		11},
+	{"^c#56B6C2^ ", "echo $(free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g)^d^",	    30,		0},
 
-	{"", "date '+%b %d %H:%M'",					5,		0},
+	{"^c#61AFEF^ ", "echo $(date '+%b %d %H:%M')^d^",					5,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
